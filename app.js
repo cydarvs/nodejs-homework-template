f067cc61edf092d8045c681c4b0f55c6e065e1fc
Cyfred Odarve <cyfredodarve@gmail.com>
2024-05-14 22:56:04 +0800

    Update App.js with code comment

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
-// Import Required Modules
+/*
+express: This is the Express.js framework for building web applications and APIs.
+logger (morgan): This is a middleware for logging HTTP requests and responses.
+cors: This middleware enables Cross-Origin Resource Sharing, which allows your server to accept requests from different origins.
+contactsRouter: This is the router module for handling routes related to contacts. It is imported from a local file.
+*/
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
 
 import { router as contactsRouter } from "./routes/api/contactsRouter.js";
 
+// Initializes a new Express application
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger));
-app.use(cors());
-app.use(express.json());
+// Use Middleware
 
+app.use(logger(formatsLogger)); // logger(formatsLogger): Uses the morgan logger middleware with the determined format.
+app.use(cors());  // cors(): Enables CORS for all routes by default.
+app.use(express.json()); // Parses incoming requests with JSON payloads.
+
+// Mounts the contactsRouter on the /api/contacts path. Any request to /api/contacts will be handled by the contactsRouter.
 app.use("/api/contacts", contactsRouter);
 
+// This middleware handles requests that do not match any route. It responds with a 404 status code 
+// and a JSON message indicating that the resource was not found.
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
+// This error-handling middleware catches any errors that occur in the application. 
+// It responds with a 500 status code and a JSON message containing the error message.
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
 // module.exports = app;
+// Exports the app instance for use in other parts of the application (such as the server file that starts the application).
 export { app };
