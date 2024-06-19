e67e729f5b12e4f6298466b5d01169fe9a4c8852
Cyfred Odarve <cyfredodarve@gmail.com>
2024-06-20 01:40:30 +0800

    Submit Homework 6

--- routes/api/usersRouter.js
+++ routes/api/usersRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { ctrlWrapper } from "../../helpers/ctrlWrapper.js";
 // prettier-ignore
-import { signupUser, loginUser, logoutUser, getCurrentUsers, updateUserSubscription, updateAvatar} from "../../controllers/usersController.js";
+import { signupUser, loginUser, logoutUser, getCurrentUsers, updateUserSubscription, updateAvatar, verifyEmail, resendVerifyEmail} from "../../controllers/usersController.js";
 import { authenticateToken } from "../../middlewares/authenticateToken.js";
 import { upload } from "../../middlewares/upload.js";
 
@@ -38,5 +38,14 @@ router.patch("/", authenticateToken, ctrlWrapper(updateUserSubscription));
 
 // prettier-ignore
 router.patch("/avatars", authenticateToken, upload.single("avatar"), ctrlWrapper(updateAvatar));
+
+/* GET: // http://localhost:3000/api/users/verify/:verificationToken */
+router.get("/verify/:verificationToken", ctrlWrapper(verifyEmail));
+
+/* POST: // http://localhost:3000/api/users/verify 
+{
+  "email": "example@example.com",
+}
+*/
 
 export { router };
